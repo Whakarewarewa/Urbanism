9cb0eb1c6098e3ed469abee84b0a19ddfec6cfa7
Eva <eve.mykhaylenko@gmail.com>
2017-03-05 16:15:41 +0200

    Update CreateMatrixC.m

--- CreateMatrixC.m
+++ CreateMatrixC.m
@@ -2,9 +2,13 @@ function [c]=CreateMatrixC(coordinates)
 n=length(coordinates);
 c=zeros(n,n);
 for i=1:n
-    for j=1:n
-        c(i, j)=GoogleMapsDistance(cell2mat(coordinates(i)), cell2mat(coordinates(j)), 'AIzaSyD5ZffsBjRdpZW1um9vBFw3LLI8_AkxV5I');
-    end
+        for j=1:n 
+            if i~=j 
+            c(i, j)=GoogleMapsDistance(cell2mat(coordinates(i)), cell2mat(coordinates(j)), 'AIzaSyD5ZffsBjRdpZW1um9vBFw3LLI8_AkxV5I');
+            else
+                c(i, i)=10000000000;
+            end
+        end
 end
 end
 
