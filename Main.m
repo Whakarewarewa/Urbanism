a3aa8f8fed52d44f2854ecf4b56869d95d573129
Eva <eve.mykhaylenko@gmail.com>
2017-03-13 20:32:09 +0200

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,12 +1,11 @@
 function [c] = Main(n)
-places_of_interest=cell(n);    
+places_of_interest=cell(n,1);    
     for i=1:n
-        places_of_interest(i)=input('Enter address please: ','s');
-        places_of_interest=char(places_of_interest); %cell/char problem...
+        places_of_interest{i}=input('Enter address please: ','s');
     end
     coordinates=cell(n, 2);
     for k=1:n
-        coordinates(k)=GoogleAddressToGeocode(places_of_interest(k),'AIzaSyD5ZffsBjRdpZW1um9vBFw3LLI8_AkxV5I');
+        coordinates{k}=GoogleAddressToGeocode(places_of_interest(k),'AIzaSyD5ZffsBjRdpZW1um9vBFw3LLI8_AkxV5I');
     end
     c=CreateMatrixC(coordinates);
 end
